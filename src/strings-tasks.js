de04d1b313fc97c101d723ee14003d94a09e0ef7
Manas Egemberdiev <manas.egemberdiev@gmail.com>
2024-11-29 17:56:39 +0600

    resolve tasks up to line 380

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -366,8 +366,14 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  return sentence
+    .split(/\s+/)
+    .reduce(
+      (longest, current) =>
+        current.length > longest.length ? current : longest,
+      ''
+    );
 }
 
 /**
